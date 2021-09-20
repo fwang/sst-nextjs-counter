6f689313bec3f1404f9bcb74ba65c48073da1d52
Frank <wangfanjie@gmail.com>
2021-09-20 16:49:07 -0400

    Add button

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,48 +1,21 @@
-import AWS from "aws-sdk";
+import { useState } from "react";
 
-export async function getServerSideProps() {
-  const dynamoDb = new AWS.DynamoDB.DocumentClient({
-    region: process.env.NEXT_PUBLIC_REGION,
-  });
+export default function Home() {
+  const [count, setCount] = useState(null);
 
-  const getParams = {
-    // Get the table name from the environment variable
-    TableName: process.env.NEXT_PUBLIC_TABLE_NAME,
-    // Get the row where the counter is called "hits"
-    Key: {
-      counter: "hits",
-    },
-  };
-  const results = await dynamoDb.get(getParams).promise();
-
-  // If there is a row, then get the value of the
-  // column called "tally"
-  let count = results.Item ? results.Item.tally : 0;
-
-  const putParams = {
-    TableName: process.env.NEXT_PUBLIC_TABLE_NAME,
-    Key: {
-      counter: "hits",
-    },
-    // Update the "tally" column
-    UpdateExpression: "SET tally = :count",
-    ExpressionAttributeValues: {
-      // Increase the count
-      ":count": ++count,
-    },
-  };
-
-  await dynamoDb.update(putParams).promise();
-
-  return {
-    props: {
-      count,
-    },
+  function onClick() {
+    fetch("/api/count", {
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then((response) => response.count)
+      .then(setCount);
   }
-}
 
-export default function Home({ count }) {
   return (
-    <p>count: { count }</p>
+    <div className="App">
+      {count && <p>You clicked me {count} times.</p>}
+      <button onClick={onClick}>Click Me!</button>
+    </div>
   )
 }
